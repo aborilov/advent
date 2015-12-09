f1b609744eef8caf3d3e2a4f899275650f93eebc
Aborilov Pavel <aborilov@gmail.com>
2015-12-09 13:17:17 +0300

    day2 part 2

--- day2.py
+++ day2.py
@@ -1,13 +1,24 @@
 lines = []
 summ = 0
+ribbon = 0
 
 
-def calc(line):
-    l, w, h = map(int, line.strip().split('x'))
+def calc(l, w, h):
     sides = [l*w, l*h, w*h]
     return sum(sides) * 2 + min(sides)
+
+
+def calc_ribon(l, w, h):
+    sides = [l, w, h]
+    sides.sort()
+    sides.pop()
+    return sum(sides)*2 + l*w*h
+
 with open('data/2') as f:
     lines = f.readlines()
 for line in lines:
-    summ += calc(line)
+    l, w, h = map(int, line.strip().split('x'))
+    summ += calc(l, w, h)
+    ribbon += calc_ribon(l, w, h)
 print summ
+print ribbon
