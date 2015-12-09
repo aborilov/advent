5f9c73c5e10d7cf64a76eb11f98e40ee40148ffe
Aborilov Pavel <aborilov@gmail.com>
2015-12-09 13:47:23 +0300

    day 3 part 2

--- day3.py
+++ day3.py
@@ -1,8 +1,9 @@
 line = None
-points = set()
-x, y = 0, 0
 with open('data/3') as f:
     line = f.readlines()[0]
+# part 1
+x, y = 0, 0
+points = set()
 for c in line:
     points.add((x, y))
     if c == '^':
@@ -13,4 +14,35 @@ for c in line:
         x += 1
     elif c == '<':
         x -= 1
+print len(points)
+
+# part 2
+x, y = 0, 0
+points = set()
+isSanta = True
+santa = (0, 0)
+robo = (0, 0)
+points.add(santa)
+for c in line:
+    x, y = 0, 0
+    if isSanta:
+        x, y = santa
+    else:
+        x, y = robo
+    if c == '^':
+        y -= 1
+    elif c == 'v':
+        y += 1
+    elif c == '>':
+        x += 1
+    elif c == '<':
+        x -= 1
+    if isSanta:
+        santa = (x, y)
+        points.add(santa)
+        isSanta = False
+    else:
+        robo = (x, y)
+        points.add(robo)
+        isSanta = True
 print len(points)
