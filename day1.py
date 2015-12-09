822a0804ddf0dce73c68582eeb02979e0c54fc60
Aborilov Pavel <aborilov@gmail.com>
2015-12-09 12:53:49 +0300

    day1 part 2

--- day1.py
+++ day1.py
@@ -3,9 +3,16 @@ floor = 0
 with open('data/1') as f:
     lines = f.readlines()
 line = lines[0]
+count = 1
+basement = 0
 for c in line:
     if c == '(':
         floor += 1
     else:
         floor -= 1
+    if not basement and floor == -1:
+        basement = count
+    else:
+        count += 1
 print floor
+print basement
